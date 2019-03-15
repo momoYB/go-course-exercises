e208fa6e3ca148b2f1fc23110a9ea7fb24c8cac1
Momchil Borisov <mib981118@gmail.com>
2019-03-15 14:22:11 +0200

    feat: Add solution for exercise 4.8

---
+++ Exercises - Ninja Level 4/Hands-on Exercise #8/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	myMap := map[string][]string{
+		"Jon Snow":           {"Knowing nothing.", "Bending the knee.", "Wildlings."},
+		"Daenerys Targaryen": {"Breaking the wheel.", "Riding dragons.", "Being called \"Queen\"."},
+		"Tyrion Lannister":   {"Drinking and knowing things.", "Trial by combat.", "Accepting what he is and wearing it like armor."},
+	}
+
+	for k, slice := range myMap {
+		fmt.Println(k)
+		for i, v := range slice {
+			fmt.Printf("%d: %v\n", i+1, v)
+		}
+		fmt.Println()
+	}
+}
